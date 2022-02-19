334417ab9665fd131d06e0a29986cd6428ae2d4e
Lucas Chagas Henrichs <84094692+henrichslucas@users.noreply.github.com>
2022-02-19 20:42:24 -0300

    UPDATES 19/02
    Efeito neon, mudança nas cores do fundo e dos elementos, botão para ligar e desligar as cores, mudança na fonte, alteração na posição dos nomes dos elementos.
    Urgente: refatorar/limpar os códigos, resolver o bug do neon "travado", aprimorar o funcionamento do botão neon, e implementar o hover quando o neon estiver desligado.

--- script.js
+++ script.js
@@ -8,84 +8,192 @@ let semiMetals = document.getElementsByName("semiMetal")
 let nonMetals = document.getElementsByName("nonMetal")
 let halogens = document.getElementsByName("halogen")
 let nobleGases = document.getElementsByName("nobleGas")
-let lanthanides = document.getElementsByName("lanthinide")
+let lanthanides = document.getElementsByName("lanthanides")
 let actinides = document.getElementsByName("actinides")
+let neonBtn = document.querySelector("#neonButton")
+
+
 
 
 // setting functions
 
-function changeBackground(selectedElement){
+
+function turnLightsOn (selectedElement){
 
     if(selectedElement == alkalineEarths){
         selectedElement.forEach(element => {
-            element.style.backgroundColor = "red"
+            
+            element.style.border = "4px solid #FB9A40"
+            element.style.color = "#FB9A40" 
         });
     }
 
     if(selectedElement == alkalineMetals){
         selectedElement.forEach(element => {
-            element.style.backgroundColor = "orange"
+            element.style.border = "4px solid #FF0000"
+            element.style.color = "#FF0000" 
         });
     }
 
     if(selectedElement == transitionMetals){
+        
         selectedElement.forEach(element => {
-            element.style.backgroundColor = "yellow"
+            element.style.border = "4px solid #ffd617"
+            element.style.color = "#ffd617"
         });
     }
 
     if(selectedElement == basicMetals){
         selectedElement.forEach(element => {
-            element.style.backgroundColor = "blue"
+            element.style.border = "4px solid #00cb1d"
+            element.style.color = "#00cb1d"
         });
     }
 
     if(selectedElement == semiMetals){
         selectedElement.forEach(element => {
-            element.style.backgroundColor = "yellowgreen"
+            element.style.border = "4px solid #00ede2"
+            element.style.color = "#00ede2"
         });
     }
 
     if(selectedElement == nonMetals){
         selectedElement.forEach(element => {
-            element.style.backgroundColor = "green"
+            element.style.border = "4px solid #EFF8FF"
+            element.style.color = "#EFF8FF"
         });
     }
 
     if(selectedElement == halogens){
         selectedElement.forEach(element => {
-            element.style.backgroundColor = "cyan"
+            element.style.border = "4px solid #9e00ff"
+            element.style.color = "#9e00ff"
         });
     }
 
     if(selectedElement == nobleGases){
         selectedElement.forEach(element => {
-            element.style.backgroundColor = "white"
+            element.style.border = "4px solid #d22cff"
+            element.style.color = "#d22cff"
         });
     }
 
     if(selectedElement == lanthanides){
         selectedElement.forEach(element => {
-            element.style.backgroundColor = "pink"
+            element.style.border = "4px solid #ff0063"
+            element.style.color = "#ff0063"
         });
     }
 
     if(selectedElement == actinides){
         selectedElement.forEach(element => {
-            element.style.backgroundColor = "salmon"
+            element.style.border = "4px solid #ff0041"
+            element.style.color = "#ff0041"
         });
     }
 
 }
 
+function turnLightsOff(selectedElement){
+    if(selectedElement == alkalineEarths){
+        selectedElement.forEach(element => {
+            
+            element.style.border = "4px solid #202020"
+            element.style.color = "#202020" 
+        });
+    }
+
+    if(selectedElement == alkalineMetals){
+        selectedElement.forEach(element => {
+            element.style.border = "4px solid #202020"
+            element.style.color = "#202020" 
+        });
+    }
+
+    if(selectedElement == transitionMetals){
+        
+        selectedElement.forEach(element => {
+            element.style.border = "4px solid #202020"
+            element.style.color = "#202020"
+        });
+    }
+
+    if(selectedElement == basicMetals){
+        selectedElement.forEach(element => {
+            element.style.border = "4px solid #202020"
+            element.style.color = "#202020"
+        });
+    }
+
+    if(selectedElement == semiMetals){
+        selectedElement.forEach(element => {
+            element.style.border = "4px solid #202020"
+            element.style.color = "#202020"
+        });
+    }
+
+    if(selectedElement == nonMetals){
+        selectedElement.forEach(element => {
+            element.style.border = "4px solid #202020"
+            element.style.color = "#202020"
+        });
+    }
+
+    if(selectedElement == halogens){
+        selectedElement.forEach(element => {
+            element.style.border = "4px solid #202020"
+            element.style.color = "#202020"
+        });
+    }
+
+    if(selectedElement == nobleGases){
+        selectedElement.forEach(element => {
+            element.style.border = "4px solid #202020"
+            element.style.color = "#202020"
+        });
+    }
+
+    if(selectedElement == lanthanides){
+        selectedElement.forEach(element => {
+            element.style.border = "4px solid #202020"
+            element.style.color = "#202020"
+        });
+    }
+
+    if(selectedElement == actinides){
+        selectedElement.forEach(element => {
+            element.style.border = "4px solid #202020"
+            element.style.color = "#202020"
+        });
+    }
+}
 
 // setting events / calling functions 
 
-changeBackground(alkalineEarths)
-changeBackground(alkalineMetals)
-changeBackground(transitionMetals)
-changeBackground(basicMetals)
-changeBackground(semiMetals)
-changeBackground(nonMetals)
-changeBackground(halogens)
-changeBackground(nobleGases)
+function inputCheck(){
+    if(neonBtn.checked == true){
+        turnLightsOn (alkalineEarths)
+        turnLightsOn (alkalineMetals)
+        turnLightsOn (transitionMetals)
+        turnLightsOn (basicMetals)
+        turnLightsOn (semiMetals)
+        turnLightsOn (nonMetals)
+        turnLightsOn (halogens)
+        turnLightsOn (nobleGases)
+        turnLightsOn (lanthanides)
+        turnLightsOn (actinides)
+    } else {
+        turnLightsOff(alkalineEarths)
+        turnLightsOff(alkalineMetals)
+        turnLightsOff(transitionMetals)
+        turnLightsOff(basicMetals)
+        turnLightsOff(semiMetals)
+        turnLightsOff(nonMetals)
+        turnLightsOff(halogens)
+        turnLightsOff(nobleGases)
+        turnLightsOff(lanthanides)
+        turnLightsOff(actinides)
+    }
+}
+
+neonBtn.addEventListener("click", inputCheck)
